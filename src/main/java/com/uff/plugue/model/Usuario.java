514337a832c4c0cff5c6217a9332c8b98e21787d
Rodolfo.Bandeira <rodolfobandeira@id.uff.br>
2021-03-30 17:49:38 -0300

    Modicação classe usuario de abstract class para class

--- src/main/java/com/uff/plugue/model/Usuario.java
+++ src/main/java/com/uff/plugue/model/Usuario.java
@@ -13,7 +13,7 @@ import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Usuario implements Serializable {
+public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
