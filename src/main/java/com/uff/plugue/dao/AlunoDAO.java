2b299b85de35e8f6806ad428922c053eb28951d3
Rodolfo.Bandeira <rodolfobandeira@id.uff.br>
2021-04-04 19:32:00 -0300

    Merge

--- src/main/java/com/uff/plugue/dao/AlunoDAO.java
+++ src/main/java/com/uff/plugue/dao/AlunoDAO.java
@@ -3,7 +3,7 @@ package com.uff.plugue.dao;
 import com.uff.plugue.model.Aluno;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; 
 
 @Repository
 public interface AlunoDAO extends JpaRepository<Aluno, Integer> {
