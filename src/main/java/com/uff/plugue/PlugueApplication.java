46f092fa1b6365de420b3b845b133105e28606b6
Milena Verissimo <milenaverissimo@gmail.com>
2021-04-04 22:11:49 -0300

    Configurando heroku

--- src/main/java/com/uff/plugue/PlugueApplication.java
+++ src/main/java/com/uff/plugue/PlugueApplication.java
@@ -2,9 +2,10 @@ package com.uff.plugue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class PlugueApplication {
+public class PlugueApplication extends WebMvcConfigurationSupport{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlugueApplication.class, args);
