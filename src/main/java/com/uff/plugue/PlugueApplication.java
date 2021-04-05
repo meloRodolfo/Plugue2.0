66597fe5a10524febd3dbd10f6dd1e182196df85
Milena Verissimo <milenaverissimo@gmail.com>
2021-04-04 22:38:21 -0300

    Subindo para heroku

--- src/main/java/com/uff/plugue/PlugueApplication.java
+++ src/main/java/com/uff/plugue/PlugueApplication.java
@@ -2,10 +2,9 @@ package com.uff.plugue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class PlugueApplication extends WebMvcConfigurationSupport{
+public class PlugueApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlugueApplication.class, args);
--- src/main/java/com/uff/plugue/service/AlunoService.java
+++ src/main/java/com/uff/plugue/service/AlunoService.java
@@ -40,5 +40,4 @@ public class AlunoService {
         proj.setAluno(aluno.get());
         projetoDao.save(proj);
     }
-
 }
