089d72e54d087ef5043cf26b9e51148da79e8f0d
Milena Verissimo <milenaverissimo@gmail.com>
2021-04-11 22:21:34 -0300

    Atualizando repositorio

--- src/main/java/com/uff/plugue/WebConfig.java
+++ src/main/java/com/uff/plugue/WebConfig.java
@@ -1,7 +1,10 @@
-package com.uff.plugue;
+// package com.uff.plugue;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter{
-    
-}
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig extends WebMvcConfigurerAdapter{
+//     @Override
+// 	public void addCorsMappings(CorsRegistry registry) {
+// 		registry.addMapping("/**");
+// 	}
+// }
