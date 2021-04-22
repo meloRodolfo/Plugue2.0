e223f92f3506d7e0183cd3a2a2e4b037c4c5770a
Gleison Lima <gleisonlima@id.uff.br>
2021-04-21 23:01:36 -0300

    Adição da notação na classe IdeiaRest para permitir a integração com o front
    This reverts commit 880b563af85f2bef60464fb3f3c26f6d9e2b51d2.

--- src/main/java/com/uff/plugue/rest/IdeiaRest.java
+++ src/main/java/com/uff/plugue/rest/IdeiaRest.java
@@ -7,6 +7,7 @@ import com.uff.plugue.model.Ideia;
 import com.uff.plugue.service.IdeiaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/ideia")
 @Api(value = "Aluno")
+@CrossOrigin(origins = "http://127.0.0.1:4200")
 public class IdeiaRest {
 
     @Autowired
